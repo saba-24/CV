b6975355301a0c9c31141c4c6f6b76ab2b407444
saba-24 <imnadzesaba7@gmail.com>
2025-04-13 15:06:22 +0400

    Add project files.

---
+++ Controllers/IncomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_test.Controllers
+{
+	public class IncomeController : Controller
+	{
+		public IActionResult Index(float i)
+		{
+			return View(i);
+		}
+	}
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvc_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
